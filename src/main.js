357608db665635f55ed4534c881fb0e125fd9001
Md. Rashad Azam Shad <mdrashadazamshad@gmail.com>
2025-11-29 21:20:01 +0600

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const utils = require("./utils");
+
+console.log("Meta Ads Tool Initialized");
+
+function analyzeAds() {
+  console.log("Analyzing Meta/Facebook Ads campaigns...");
+  // Add Meta Ads API logic here
+}
+
+analyzeAds();
